fb59cef4ff32b4f9aa56d09004de2ee0e3a47259
yugha13 <yughas83@gmail.com>
2026-07-11 11:55:39 +0530

    refactor: remove unused imports and eslint disable directive from hackathons section

--- src/components/section/hackathons-section.tsx
+++ src/components/section/hackathons-section.tsx
@@ -1,9 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
-import { Badge } from "@/components/ui/badge";
-import Link from "next/link";
-import { DATA } from "@/data/resume";
-import { Timeline, TimelineItem, TimelineConnectItem } from "@/components/timeline";
-
 export default function HackathonsSection() {
   return (
     <section id="hackathons" className="overflow-hidden">
